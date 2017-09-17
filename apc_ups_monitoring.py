20e6ef002872abe85f8339d1fc37c565178a2221
CaeSium <cs@district.at>
2017-09-17 10:43:46 +0200

    Initial version

---
+++ apc_ups_monitoring.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+""" Script to poll the UPS (via apcupsd) and pushong the data to statsd server
+    to view the data in an Graphite/Grafana environment.
+"""
+
+import subprocess
+import statsd
+import config as cfg
+
+
+apc_status = {}
+
+# which status messages to publish. We use upsname as part of the topic
+interesting = ('battv', 'linev', 'loadpct', 'mbattchg', 'timeleft')
+# apcaccess command
+command = ['apcaccess', 'status']
+
+
+statsd_client_ups = statsd.StatsClient(host=cfg.statsd_config['host'],
+                                       port=cfg.statsd_config['port'], prefix="ups")
+
+proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+while True:
+    # get STDOUT of process
+    stdout = proc.stdout.readline().decode(errors='ignore')
+
+    (key, spl, val) = stdout.partition(': ')
+    key = key.rstrip().lower()
+    val = val.split(' ')[0]
+    apc_status[key] = val
+
+    if stdout == '' and proc.poll() is not None:
+        break
+
+# Process returned an error
+if proc.returncode != 0:
+    error = "%s failed (%s)" % ("APC_Status", ' '.join(command))
+    raise Exception(error)
+
+# send values to statsd server
+for values in interesting:
+    statsd_client_ups.gauge(values, float(apc_status[values]), rate=1, delta=False)
+
---
+++ config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""Configuration file for BlackDNS Updater
+
+Attributes:
+    statsd_config (dict): Contains server infos for Statsd server
+"""
+
+statsd_config = {'host': 'localhost',
+                 'port': '8125'}
